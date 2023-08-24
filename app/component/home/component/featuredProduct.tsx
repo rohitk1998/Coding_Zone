7a49f9a129d3cddaa190015c5af62cd325cd77c9
Rohit Kumar <rohit.kumar@antiersolutions.com>
2023-08-24 22:50:24 +0530

    prduct section done

--- app/component/home/component/featuredProduct.tsx
+++ app/component/home/component/featuredProduct.tsx
@@ -17,7 +17,7 @@ const FeaturedProduct = () => {
       "
     >
       <div className="w-[75%] mx-auto flex flex-col items-center xl:justify-center lg:justify-cetner justify-center rounded-[20px]">
-        <h1 className="mb-4 text-3xl font-bold text-black md:text-3xl lg:text-4xl ml-2">
+        <h1 className="mb-4 text-3xl font-bold text-black md:text-3xl lg:text-4xl ml-2 text-center">
           Produced Product
         </h1>
         <h1 className="mb-4 text-xl font-normal text-black md:text-2xl lg:text-2xl ml-2 text-center">
--- app/component/home/component/productCrousal.tsx
+++ app/component/home/component/productCrousal.tsx
@@ -227,7 +227,7 @@ const ProductSection = () => {
           </div> */}
     <section className="py-10">
       <div className="container mx-auto px-4">
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 justify-center">
           {/* Product Cards */}
           {/* Create your product cards here */}
           {data.map((product, index) => (
