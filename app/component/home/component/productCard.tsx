893ed50224d5ac6e9a8ae09ee6c7775bc673b20f
Rohit Kumar <rohit.kumar@antiersolutions.com>
2023-08-26 16:59:23 +0530

    changes

--- app/component/home/component/productCard.tsx
+++ app/component/home/component/productCard.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import Image from "next/image";
 import React from "react";
 
 interface Props {
@@ -9,7 +10,7 @@ interface Props {
 const ProductCard: React.FC<Props> = ({ product }) => {
   return (
     <div className="group border-[1px] relative lg:w-[100%] md:w-[100%] w-[100%] bg-white overflow-hidden rounded-2xl hover:rounded-2xl hover:-translate-y-1 hover:scale-110 hover:bg-primaryColor duration-200 hover:text-white text-black">
-      <img
+      <Image
         src={product.image}
         alt={product.name}
         className="w-full h-40 object-cover mb-4"
