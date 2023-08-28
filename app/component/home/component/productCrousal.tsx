29a70dd01a30cc79ef7ed6974b8404991b4feb33
Rohit Kumar <rohit.kumar@antiersolutions.com>
2023-08-29 02:17:39 +0530

    info badge fixes and footer address text fixes to small fonts

--- app/component/home/component/productCrousal.tsx
+++ app/component/home/component/productCrousal.tsx
@@ -7,7 +7,7 @@ const ProductSection = () => {
         <div className="flex flex-row items-center justify-around w-[100%] p-0 m-0">
     <section className="py-10">
       <div className="container mx-auto px-4">
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 justify-center">
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-4 gap-10 justify-center">
           {data.map((product, index) => (
           <ProductCard key={index} product={product} />
         ))}
